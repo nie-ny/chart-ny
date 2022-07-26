ac1b687a9a2836418f708c5de27c82957b737d7d
ny <niey@tianji.cn>
2022-07-26 16:32:17 +0800

    chore: 初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     'jest/globals': true
   },
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
@@ -13,6 +14,7 @@ module.exports = {
     // 这里添加一行规则把这条规则隐藏
     'import/prefer-default-export': 0,
     'comma-dangle': 0,
-    semi: 0
+    semi: 0,
+    'prettier/prettier': 0
   }
 }
